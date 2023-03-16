57f8e6d30ed25d452a7605d21e1189e8e8e2e127
Diego Rojo <Diego99rojo@gmail.com>
2023-03-16 13:21:45 +0000

    Back to previous commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 
+function LoginApp() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          CRUD operations
+        </p>
+      </header>
+    </div>
+  );
+}
+
+//FillEmployee
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Listing Page
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body class="App-body">
+      <table>
+          <tr>
+            <th>Employee ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Date of Birth</th>
+            <th>Email</th>
+            <th>Skill Level</th>
+            <th>Active</th>
+            <th>Age</th>
+          </tr>
+          
+        </table>
+      </body>
     </div>
   );
+}
+
+function FillEmployee() {
+  return(
+    <tr>
+      <td>Employee ID</td>
+      <td>First Name</td>
+      <td>Last Name</td>
+      <td>Date of Birtd</td>
+      <td>Email</td>
+      <td>Skill Level</td>
+      <td>Active</td>
+      <td>Age</td>
+    </tr>
+  )
 }
 
 export default App;
