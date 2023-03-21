d2ac21d5931bef4e3dd6676a467392553b7a8ebc
Diego Rojo <Diego99rojo@gmail.com>
2023-03-21 15:44:27 +0000

    Token needed for api calls work in Postman

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const app = express();
 var mysql = require('mysql');
 
+const jwt= require('jsonwebtoken');
+require('dotenv').config();
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -14,17 +17,42 @@ var connection = mysql.createConnection({
 app.use(cors());
 app.use(express.json());
 
+function generateAccessToken(username){
+  return jwt.sign(username, process.env.TOKEN_SECRET, {expiresIn:'30s'});
+}
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err)
+
+    if (err) return res.sendStatus(403)
+
+    req.user = user
+
+    next()
+  })
+}
+
 app.post('/api/Authenticate', (req, res) => {
 
   let authJson=req.body;
   let u=authJson.username;
   let p=authJson.password;
   let auth = [u,p];
+
   
   connection.query("SELECT * FROM users WHERE username=? AND password=?;",auth, 
   (err, results, fields) => {
     if(err) throw err;
-      res.send(results);
+    
+      const token = generateAccessToken({username:u});
+      res.json({token});
+     // res.send(currentUser);
   });
 });
 
@@ -33,7 +61,6 @@ app.post('/api/Employees', (req, res) => {
     connection.query("INSERT INTO employees SET?;",dataEmployee, 
     (err, results, fields) => {
       if(err) throw err;
-      //SEND JWT
         res.send(results);
     });
   });
@@ -45,7 +72,7 @@ app.post('/api/Employees', (req, res) => {
     });
   });
 
-  app.get('/api/Employees', (req, res) => {
+  app.get('/api/Employees',authenticateToken, (req, res) => {
     connection.query("SELECT * FROM employees;", (err, results, fields) => {
       if(err) throw err;
       res.send(results);
