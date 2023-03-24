8d5f50525faa59ea07b80a6a819ee5c76428d833
Diego Rojo <Diego99rojo@gmail.com>
2023-03-24 14:46:12 +0000

    Frontend changes
    Home button added and confirm dialog when logging out

--- src/App.js
+++ src/App.js
@@ -11,7 +11,15 @@ function App() {
     <div className="App">
       <div className="App-header">
           <div id="app-name">
-            <h2 onClick={listing}>CRUD APP</h2>
+            <h2 id="home" onClick={listing}> 
+            <svg viewBox="0 0 512 512" fill="currentColor" height="1em" width="1em"  >
+              <path d="M261.56 101.28a8 8 0 00-11.06 0L66.4 277.15a8 8 0 00-2.47 5.79L63.9 448a32 32 0 0032 32H192a16 16 0 0016-16V328a8 8 0 018-8h80a8 8 0 018 8v136a16 16 0 0016 16h96.06a32 32 0 0032-32V282.94a8 8 0 00-2.47-5.79z" />
+              <path d="M490.91 244.15l-74.8-71.56V64a16 16 0 00-16-16h-48a16 16 0 00-16 16v32l-57.92-55.38C272.77 35.14 264.71 32 256 32c-8.68 0-16.72 3.14-22.14 8.63l-212.7 203.5c-6.22 6-7 15.87-1.34 22.37A16 16 0 0043 267.56L250.5 69.28a8 8 0 0111.06 0l207.52 198.28a16 16 0 0022.59-.44c6.14-6.36 5.63-16.86-.76-22.97z" />
+            </svg>
+           </h2>
+           <h2 id="h2-name">
+            CRUD APP
+           </h2>
           </div>
           <div id="user-header">
             <div id="user-buttons">
@@ -153,10 +161,14 @@ function listing(){
 }
 
 function logOut(){
-  var date = new Date();
-  date.setTime(date.getTime());
-  document.cookie="token=''; expires="+date.toGMTString();
-  window.location.reload();
+  if(window.confirm("Do you want to log out?")){
+    var date = new Date();
+    date.setTime(date.getTime());
+    document.cookie="token=''; expires="+date.toGMTString();
+    window.location.reload();
+  }else{
+
+  }
 }
 
 
